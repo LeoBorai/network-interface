0af2740f14111061e2d153f7277be9b89438a66f
Sinh Pham <phamansinh@gmail.com>
2025-08-07 19:44:37 -0700

    tvos

--- src/target/unix/ffi/mod.rs
+++ src/target/unix/ffi/mod.rs
@@ -3,6 +3,7 @@ use libc::ifaddrs;
 #[cfg(any(
     target_os = "macos",
     target_os = "ios",
+    target_os = "tvos",
     target_os = "freebsd",
     target_os = "openbsd",
     target_os = "netbsd",
